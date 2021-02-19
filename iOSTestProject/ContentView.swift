574f7331acccec7f4dec01ace12ef4ffc4c6ffaf
dos Santos, Diego <diego.dos.santos@sap.com>
2021-02-18 22:54:25 -0300

    Finished the logic

--- iOSTestProject/ContentView.swift
+++ iOSTestProject/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
     @State private var checkInDate = Date()
     @State private var checkOutDate = Date()
     @State private var isRewardClient = false
+    @State private var hotel: Hotel?
 
     var body: some View {
         ScrollView {
@@ -30,7 +31,11 @@ struct ContentView: View {
                 ).labelsHidden()
 
                 Button("Best hotel") {
-                    self.getBestHotel(checkInDate: checkInDate, checkOutDate: checkOutDate, isReward: isRewardClient)
+                    self.hotel = self.getBestHotel(checkInDate: checkInDate, checkOutDate: checkOutDate, isReward: isRewardClient)
+                }
+                
+                if let hotel = self.hotel {
+                    Text(hotel.name)
                 }
             }.padding(16)
         }
@@ -44,8 +49,23 @@ extension ContentView {
     }
     
     private func getDays(checkInDate: Date, checkOutDate: Date) -> (Int, Int) {
+        guard checkInDate <= checkOutDate else {
+            return (0,0)
+        }
+        var weekend = 0
+        var week = 0
+        var date = checkInDate
         
-        return (0,0)
+        repeat {
+            if Calendar.current.isDateInWeekend(date) {
+                weekend +=  1
+            } else {
+                week += 1
+            }
+            date = Calendar.current.date(byAdding: .day, value: 1, to: date) ?? date
+        } while date <= checkOutDate
+        
+        return (week,weekend)
     }
 }
 
